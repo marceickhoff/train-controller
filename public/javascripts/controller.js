3c847041c6542ab64c5f35cb0574fb5a39f114db
marceickhoff <mail@marceickhoff.com>
2019-09-15 14:10:16 +0200

    Frontend and loco stuff

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,9 +1,38 @@
 var socket = io();
 
-var send = function(data) {
+function updatePanel(data) {
+	// not supported yet
+}
+
+function selectPanel(name) {
+	window.location.href = window.location.host + '/' + name;
+}
+
+function toggleFullscreen() {
+	if (document.fullScreenElement || (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+		if (document.documentElement.requestFullScreen) {
+			document.documentElement.requestFullScreen();
+		} else if (document.documentElement.mozRequestFullScreen) {
+			document.documentElement.mozRequestFullScreen();
+		} else if (document.documentElement.webkitRequestFullScreen) {
+			document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+		}
+	}
+	else {
+		if (document.cancelFullScreen) {
+			document.cancelFullScreen();
+		} else if (document.mozCancelFullScreen) {
+			document.mozCancelFullScreen();
+		} else if (document.webkitCancelFullScreen) {
+			document.webkitCancelFullScreen();
+		}
+	}
+}
+
+function send(data) {
 	socket.emit('send', data);
-};
+}
 
 socket.on('update', function(data) {
-	//TODO
+	updatePanel(data);
 });
